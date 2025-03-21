4fcc63007c17969a3ff254fdf196ceede068c72f
AlessandroC <p4cco22@gmail.com>
2025-03-21 22:06:33 +0100

    primo commit

---
+++ base.js
@@ -0,0 +1,25 @@
+function isEvenOrOdd(inputNumber) {
+    let result;
+
+    if (inputNumber % 2 ==0) {
+        result="pari";
+    } else {
+        result="dispari";
+    }
+    console.log("Il numero "+inputNumber+" ae' " + result)
+}
+
+isEvenOrOdd(6);
+isEvenOrOdd(7);
+
+let myString="arcobalenooo"; 
+console.log("La lunghezza della stringa e' "+myString.length);
+
+let numVocali=0;
+for (c=0;c<myString.length;c++) {
+    if (myString[c]=='a' || myString[c]=='e' || myString[c]=='i' || myString[c]=='o' || myString[c]=='u'  || 
+        myString[c]=='A' || myString[c]=='E' || myString[c]=='I' || myString[c]=='O' || myString[c]=='U' )
+        numVocali++;
+} 
+console.log(`Il numero delle vocali nella parola ${myString} e': `+numVocali);
+
